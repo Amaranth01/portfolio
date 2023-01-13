b276c0c671bf6f9e2bf64d5f7cca6d1888276da4
Amaranth01 <vanessa.nectoux@gmail.com>
2023-01-13 14:53:07 +0100

    Main page completed

--- View/base.html.php
+++ View/base.html.php
@@ -29,15 +29,46 @@ if (isset($_SESSION['success'])) {
 ?>
 
 <h1>Vanessa Nectoux</h1>
-<div class="arrow" id="arrowLeft">
-     <a href="" title="Parcours professionnel" class="left"> <span ><</span> </a>
-</div>
-<div class="arrow" id="arrowRight">
-    <a href="" title="Outils maîtrisé et compétences technique" class="right"><span >  > </span></a>
-</div>
+<a href="" title="Parcours" target="_blank">
+    <div class="arrow arrowLeft">
+        <p id="parcours">
+            P <br>
+            A <br>
+            R <br>
+            C <br>
+            O <br>
+            U <br>
+            R <br>
+            S <br>
+            <br>
+        </p>
+    </div>
+</a>
+
+<a href=""  title="Projets">
+    <div class="arrow arrowRight">
+        <p id="project">
+            P <br>
+            R <br>
+            O <br>
+            J <br>
+            E <br>
+            T <br>
+            S <br>
+        </p>
+    </div>
+</a>
 <div>
     <p id="intro">Je suis développeuse web full-stack, ainsi que conceptrice et développeuse d'application.</p>
     <img src="/img/283071454_8262947760397701_1820033294722123302_n.jpg" alt="Portrait de moi" id="portrait">
+</div>
+
+<div id="aboutMe">
+    <p>
+        Je vous présente mon portfolio qui retrace mon parcours professionnel. J'ai toujours eu soif d'en savoir plus,
+        il n'y a rien de pire que de ne plus apprendre. Le développement web, ainsi que la conception d'application
+        ont été une évidence pour moi.
+    </p>
 </div>
 
 <main>
@@ -46,12 +77,18 @@ if (isset($_SESSION['success'])) {
 
 <div id="link">
     <a href="https://github.com/Amaranth01" target="_blank"><i class="fa-brands fa-github"></i></a>
-    <a href="https://www.linkedin.com/in/vanessa-nectoux-27b490179/"><i class="fa-brands fa-linkedin"></i></a>
+    <a href="https://www.linkedin.com/in/vanessa-nectoux-27b490179/" target="_blank"><i
+                class="fa-brands fa-linkedin"></i></a>
+    <a href="" target="_blank" title="Contact"><i class="fa-solid fa-address-card"></i></a>
 </div>
 
-<div class="arrow" id="arrowDown">
-    <a href="" title="Projets" class="down"><p > > </p></a>
-</div>
+<a href="" title="Outils maîtrisé et compétences technique" target="_blank" class="down">
+    <div class="arrow arrowDown">
+        <p id="utils">
+            OUTILS ET COMPETENCES
+        </p>
+    </div>
+</a>
 
 <script src="https://kit.fontawesome.com/f33464c73a.js" crossorigin="anonymous"></script>
 <script src="build/js/front-bundle.js"></script>
