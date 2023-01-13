8c902d8256e210e5bd72b18552e1d820d239e3a8
Amaranth01 <vanessa.nectoux@gmail.com>
2023-01-13 11:37:31 +0100

    Continue the base

--- View/base.html.php
+++ View/base.html.php
@@ -27,16 +27,14 @@ if (isset($_SESSION['success'])) {
     </div> <?php
 }
 ?>
-<nav>
-    <ul>
-        <li><a href="">Présentation</a></li>
-        <li><a href="">Compétences technique et langue</a></li>
-        <li><a href="">Outils maîtrisés</a></li>
-        <li><a href="">Exemples de projets</a></li>
-    </ul>
-</nav>
 
 <h1>Vanessa Nectoux</h1>
+<div class="arrow" id="arrowLeft">
+     <a href="" title="Parcours professionnel" class="left"> <span ><</span> </a>
+</div>
+<div class="arrow" id="arrowRight">
+    <a href="" title="Outils maîtrisé et compétences technique" class="right"><span >  > </span></a>
+</div>
 <div>
     <p id="intro">Je suis développeuse web full-stack, ainsi que conceptrice et développeuse d'application.</p>
     <img src="/img/283071454_8262947760397701_1820033294722123302_n.jpg" alt="Portrait de moi" id="portrait">
@@ -46,6 +44,16 @@ if (isset($_SESSION['success'])) {
     <?= $html ?>
 </main>
 
+<div id="link">
+    <a href="https://github.com/Amaranth01" target="_blank"><i class="fa-brands fa-github"></i></a>
+    <a href="https://www.linkedin.com/in/vanessa-nectoux-27b490179/"><i class="fa-brands fa-linkedin"></i></a>
+</div>
+
+<div class="arrow" id="arrowDown">
+    <a href="" title="Projets" class="down"><p > > </p></a>
+</div>
+
+<script src="https://kit.fontawesome.com/f33464c73a.js" crossorigin="anonymous"></script>
 <script src="build/js/front-bundle.js"></script>
 </body>
 </html>
